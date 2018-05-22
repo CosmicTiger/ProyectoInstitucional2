55414bbff7f9a0883e4cea57e4a81888ff583714
CosmicTiger <luisangelmarcia@gmail.com>
2018-05-21 22:21:05 -0600

    Correcciones

--- ProyectoInstitucional/src/Pojo/Credencial.java
+++ ProyectoInstitucional/src/Pojo/Credencial.java
@@ -10,5 +10,41 @@ package Pojo;
  * @author kevin
  */
 public class Credencial {
-    
+    private int IdCredencial;
+    private String User;
+    private String Pass;
+
+    public Credencial() {
+    }
+
+    public Credencial(int IdCredencial, String User, String Pass) {
+        this.IdCredencial = IdCredencial;
+        this.User = User;
+        this.Pass = Pass;
+    }
+
+    public int getIdCredencial() {
+        return IdCredencial;
+    }
+
+    public void setIdCredencial(int IdCredencial) {
+        this.IdCredencial = IdCredencial;
+    }
+
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String User) {
+        this.User = User;
+    }
+
+    public String getPass() {
+        return Pass;
+    }
+
+    public void setPass(String Pass) {
+        this.Pass = Pass;
+    }
+
 }
