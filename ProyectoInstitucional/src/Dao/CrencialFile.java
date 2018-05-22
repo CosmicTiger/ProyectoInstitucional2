9e55d712de03cd8f23313b7a08abd382305bfaa6
LAPTOP-MF23959V\kevin <kevinagb97@gmail.com>
2018-05-21 22:19:03 -0600

    Pojo

--- ProyectoInstitucional/src/Dao/CrencialFile.java
+++ ProyectoInstitucional/src/Dao/CrencialFile.java
@@ -10,5 +10,43 @@ package Dao;
  * @author kevin
  */
 public class CrencialFile {
+
+    private int IdCredencial;
+    private String User;
+    private String Pass;
+
+    public CrencialFile() {
+    }
+
+    public CrencialFile(int IdCredencial, String User, String Pass) {
+        this.IdCredencial = IdCredencial;
+        this.User = User;
+        this.Pass = Pass;
+    }
+
+    public int getIdCredencial() {
+        return IdCredencial;
+    }
+
+    public void setIdCredencial(int IdCredencial) {
+        this.IdCredencial = IdCredencial;
+    }
+
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String User) {
+        this.User = User;
+    }
+
+    public String getPass() {
+        return Pass;
+    }
+
+    public void setPass(String Pass) {
+        this.Pass = Pass;
+    }
+
     
 }
--- ProyectoInstitucional/src/Pojo/Student.java
+++ ProyectoInstitucional/src/Pojo/Student.java
@@ -10,5 +10,73 @@ package Pojo;
  * @author kevin
  */
 public class Student {
+
+    private int IdStudent;
+    private String Name;
+    private String LastName;
+    private String DocId;
+    private String Phone;
+    private String Address;
+
+    public Student() {
+    }
+
+    public Student(int IdStudent, String Name, String LastName, String DocId, String Phone, String Address) {
+        this.IdStudent = IdStudent;
+        this.Name = Name;
+        this.LastName = LastName;
+        this.DocId = DocId;
+        this.Phone = Phone;
+        this.Address = Address;
+    }
+
+    public int getIdStudent() {
+        return IdStudent;
+    }
+
+    public void setIdStudent(int IdStudent) {
+        this.IdStudent = IdStudent;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getDocId() {
+        return DocId;
+    }
+
+    public void setDocId(String DocId) {
+        this.DocId = DocId;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
     
 }
